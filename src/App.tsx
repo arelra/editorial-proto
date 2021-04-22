87c9970705c3825187c445b7c0c6c4211912c7b2
Joe Cowton <joe.cowton@guardian.co.uk>
2021-04-22 16:38:45 +0100

    add filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import { Header } from './header/Header';
-import { Nav } from './nav/Nav';
-import {Page} from './page/Page';
-import './App.css'
+import React from "react";
+import { Header } from "./header/Header";
+import { Nav } from "./nav/Nav";
+import { Page } from "./page/Page";
+import "./App.css";
+import FilterContextWrapper from "./filterContext/filterContext";
+
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Nav />
-	<Page />
-    </div>
-  )
+	return (
+		<div className="App">
+			<FilterContextWrapper>
+				<Header />
+				<Nav />
+				<Page />
+			</FilterContextWrapper>
+		</div>
+	);
 }
 
 export default App;
